c0cd3c74584f5a60331fc07d0ed38856ae9eaf45
sbartel <samuel.bartel@orange.com>
2015-06-09 09:27:53 +0200

    Add  multi backend support
    Add missing dependencies on compute node
    Add capacity to deploy cinder role on a non controller node
    
    Change-Id: Ibc5954f4309dd9198752d5cec701a6187c4f8a6d

---
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/multibackend_cinder.pp
@@ -0,0 +1,14 @@
+# this can be used to insert into cinder_iscsi section lvm configuration parameters
+#
+
+class plugin_cinder_netapp::multibackend_cinder
+inherits plugin_cinder_netapp::params { 
+    include cinder::params
+
+    class { $plugin_cinder_netapp::params::backend_class :} ->
+    plugin_cinder_netapp::backend::netapp{ "cinder_netapp":
+        backends      => $plugin_cinder_netapp::params::backends,
+        mutlibackends => true,
+        cinder_node        => true
+    }
+}
---
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/params.pp
@@ -0,0 +1,37 @@
+class plugin_cinder_netapp::params
+{
+    include cinder::params
+    $cinder_hash  = $::fuel_settings['cinder']
+    $storage_hash = $::fuel_settings['storage']
+
+    case $::osfamily {
+      'Debian': {
+        package { 'nfs-common':
+          before => Cinder::Backend::Netapp['cinder_netapp'],
+        }
+      }
+      'RedHat': {
+        package { 'nfs-utils': } ->
+        service {'rpcbind':
+          ensure => running,
+        } ->
+        service {'rpcidmapd':
+          ensure => running,
+        } ->
+        service {'nfs':
+          ensure => running,
+          before => Cinder::Backend::Netapp['cinder_netapp'],
+        }
+      }
+      default: {
+        fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} currently only supports osfamily RedHat and Debian")
+      }
+    }
+    if ($storage_hash['volumes_lvm']) {
+      $backends      = 'cinder_isci'
+      $backend_class = 'plugin_cinder_netapp::backend::iscsi'
+    } elsif ($storage_hash['volumes_ceph']) {
+      $backends = 'cinder_rbd'
+      $backend_class = 'plugin_cinder_netapp::backend::rbd'
+    }
+}
