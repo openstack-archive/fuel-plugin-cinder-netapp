590ba6ce0ba65aa4fe42042f85919c293ca58f25
Andrey Volochay <avolochay@mirantis.com>
2015-09-21 20:03:26 +0300

    Add E-Series storage familly support
    - added E-Series storage family support
    - the plugin UI was refactored for better visibility
    - fields screenshots were updated
    
    Change-Id: Ibcc9dd51a86803e45ed334a46d97f29224e5ba5c

--- deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/netapp.pp
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/netapp.pp
@@ -11,36 +11,34 @@
 #
 define plugin_cinder_netapp::backend::netapp (
   $section       = $name,
-  $backends      ="",
+  $backends      = "",
   $mutlibackends = false,
-  $cinder_node        = false
+  $cinder_node   = false
 )  {
     include cinder::client
     #Ensure that $ symbole is correctly escaped in netapp password
     $netapp_password = regsubst($::fuel_settings['cinder_netapp']['netapp_password'],'\$','$$','G')
-    $backend_name='cinder_netapp'
+    $backend_name = 'cinder_netapp'
     cinder::backend::netapp { "${backend_name}":
+      backend_name                 => $section,
       netapp_login                 => $::fuel_settings['cinder_netapp']['netapp_login'],
       netapp_password              => $netapp_password,
       netapp_server_hostname       => $::fuel_settings['cinder_netapp']['netapp_server_hostname'],
-      volume_backend_name          => $section,
       netapp_server_port           => $::fuel_settings['cinder_netapp']['netapp_server_port'],
-      netapp_size_multiplier       => $::fuel_settings['cinder_netapp']['netappsize_multiplier'],
+      netapp_transport_type        => $::fuel_settings['cinder_netapp']['netapp_transport_type'],
       netapp_storage_family        => $::fuel_settings['cinder_netapp']['netapp_storage_family'],
       netapp_storage_protocol      => $::fuel_settings['cinder_netapp']['netapp_storage_protocol'],
-      netapp_transport_type        => $::fuel_settings['cinder_netapp']['netapp_transport_type'],
+      netapp_vserver               => $::fuel_settings['cinder_netapp']['netapp_vserver'],
+      netapp_controller_ips        => $::fuel_settings['cinder_netapp']['netapp_controller_ips'],
+      netapp_sa_password           => $::fuel_settings['cinder_netapp']['netapp_sa_password'],
+      netapp_storage_pools         => $::fuel_settings['cinder_netapp']['netapp_storage_pools'],
+      netapp_size_multiplier       => $::fuel_settings['cinder_netapp']['netapp_size_multiplier'],
       netapp_vfiler                => $::fuel_settings['cinder_netapp']['netapp_vfiler'],
       netapp_volume_list           => $::fuel_settings['cinder_netapp']['netapp_volume_list'],
-      netapp_vserver               => $::fuel_settings['cinder_netapp']['netapp_vserver'],
       expiry_thres_minutes         => $::fuel_settings['cinder_netapp']['expiry_thres_minutes'],
       thres_avl_size_perc_start    => $::fuel_settings['cinder_netapp']['thres_avl_size_perc_start'],
       thres_avl_size_perc_stop     => $::fuel_settings['cinder_netapp']['thres_avl_size_perc_stop'],
-      nfs_shares_config            => "/etc/cinder/shares.conf",
       netapp_copyoffload_tool_path => $::fuel_settings['cinder_netapp']['netapp_copyoffload_tool_path'],
-      #netapp_controller_ips        => '',
-      #netapp_sa_password           => '',
-      #netapp_storage_pools         => '',
-      #netapp_webservice_path       => '/devmgr/v2',
     }
     $index = $::fuel_settings['cinder_netapp']['nb_share']
     if $mutlibackends{
