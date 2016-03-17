d1ad8be425a4c1035c195f1bf92d723903cd9ff5
Andrey Volochay <avolochay@mirantis.com>
2016-03-17 18:38:33 +0300

    Add MOS 8.0 support
     - The plugin has been totally refactored to accordance with changes in
       OpenStack Liberty and MOS 8.0
     - All documented features of NetApp Cinder Driver are configurable
     - Plugin can change Cinder settings after deployment with wrong
       parameters. It requires manual actions
     - Defined type (backend::netapp) form upstream puppet cinder module is
       not used because is outdated and doesn't support last changes in
       Cinder NetApp driver
     - Added a tasks that deletes not existed cinder services
    
    Change-Id: Ic70ad0a99c41860502e15fe8671e6ca3f11e9f58

---
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -0,0 +1,23 @@
+notice('MODULAR: netapp-compute')
+
+include nova::params
+
+$cinder_netapp = hiera_hash('cinder_netapp', {})
+
+if ($cinder_netapp['netapp_storage_protocol']) == 'iscsi' and ($cinder_netapp['use_multipath_for_image_xfer']) {
+  package { 'multipath-tools': }
+
+  nova_config {
+    'libvirt/iscsi_use_multipath': value => True;
+  } ~> service { "$nova::params::compute_service_name": }
+}
+
+if ($cinder_netapp['netapp_storage_protocol']) == 'nfs' {
+  package { 'nfs-common': }
+
+  if $cinder_netapp['nfs_mount_options'] {
+    nova_config {
+      'libvirt/nfs_mount_options': value => $cinder_netapp['nfs_mount_options'];
+    } ~> service { "$nova::params::compute_service_name": }
+  }
+}
