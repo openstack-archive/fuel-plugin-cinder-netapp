2f86108101ca5dc092cd249fa34e4ee4d7fa7a08
Andrey Volochay <avolochay@mirantis.com>
2016-03-17 18:38:33 +0300

    Add MOS 8.0 support
     - The plugin has been totally refactored to accordance with changes in
       OpenStack Liberty and MOS 8.0
     - All documented features of NetApp Cinder Driver are configurable
     - Plugin can change Cinder settings after deployment with wrong
       parameters. It requires manual actions
     - Defined type (backend::netapp) form upstream puppet cinder module is
       not used because is outdated and doesn't support last changes in
       Cinder NetApp driver
     - Added a tasks that deletes not existed cinder services
    
    Change-Id: Ic70ad0a99c41860502e15fe8671e6ca3f11e9f58

--- deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/iscsi.pp
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/iscsi.pp
@@ -1,28 +1,20 @@
-# this can be used to insert into cinder_iscsi section lvm configuration parameters
-#
-# [iscsi_ip_address] The IP address that the iSCSI daemon is listening on
-# [iscsi_helper] iSCSI target user-land tool to use
-# [volume_group] Name for the VG that will contain exported volumes
-#
-# === Examples
-#
-#  class { 'plugin_cinder_netapp::backend::iscsi':
-#  $iscsi_ip_address = '127.0.0.1',
-#  $iscsi_helper,
-#  $volume_group     = 'cinder' ,
-#  }
-#
 class plugin_cinder_netapp::backend::iscsi (
   $volume_group     = 'cinder',
-  $iscsi_ip_address =  $::internal_address,
-  $iscsi_helper     =  $::cinder::params::iscsi_helper,
-) {
+  $iscsi_helper     = $::cinder::params::iscsi_helper,
+  $backend_type,
+) inherits cinder::params {
+
+  $network_scheme = hiera_hash('network_scheme', {})
+  prepare_network_config($network_scheme)
+
+  $storage_address  = get_network_role_property('cinder/iscsi', 'ipaddr')
 
   cinder_config {
-    'cinder_iscsi/volume_backend_name': value => 'cinder_iscsi';
-    'cinder_iscsi/iscsi_ip_address':    value => $iscsi_ip_address;
+    'cinder_iscsi/volume_backend_name': value => $backend_type;
+    'cinder_iscsi/volume_driver':       value => 'cinder.volume.drivers.lvm.LVMVolumeDriver';
     'cinder_iscsi/iscsi_helper':        value => $iscsi_helper;
     'cinder_iscsi/volume_group':        value => $volume_group;
+    'cinder_iscsi/iscsi_ip_address':    value => $storage_address;
+    'cinder_iscsi/backend_host':        value => $storage_address;
   }
-
 }
