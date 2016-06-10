da25912a1965afc441d0dfe18db91df1a6bbed9b
Andrey Volochay <avolochay@mirantis.com>
2016-06-10 18:02:53 +0300

    Add full support of multi backends
     - Added full support of multi backends. The plugin does not overwrite
     "enabled_backends" option that allows to use it with other
     plugins for Cinder backends.
     - Since FilterScheduler is default scheduler driver starting Grizzly,
     it has been deleted from set up. This change allows to set Cinder or
     Ceph-OSD roles wherever you need.
     - Added small changes in site.pp to get free allocation of roles
     - The code has been refactored and commented, slightly.
    
    Closes-bug: 1581960
    
    Change-Id: I117fed67553af4065b3ebd3673a17b51cf42dc9a

--- README.md
+++ README.md
@@ -27,6 +27,14 @@ Requirements
 Release Notes
 -------------
 
+**4.2.0**
+
+* Added full support of multi backends
+
+* The code has been refactored and commented, slightly
+
+* Added small changes to get free allocation of roles
+
 **4.1.1**
 
 * Fix bug with multibackend when Ceph is used (bug: 1581028)
--- deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/netapp.pp
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/netapp.pp
@@ -1,34 +1,30 @@
 # Defined type form upstream puppet cinder module is not used because is outdated and doesn't support last changes in Cinder NetApp driver.
 
-define plugin_cinder_netapp::backend::netapp (
-  $netapp_backend_name = $name,
-  $cinder_netapp       = $plugin_cinder_netapp::params::cinder_netapp,
-  $backend_name        = undef,
-  $nfs_shares_config   = '/etc/cinder/shares.conf',
+class plugin_cinder_netapp::backend::netapp (
+  $netapp_backend_name    = 'cinder_netapp',
+  $cinder_netapp          = $plugin_cinder_netapp::params::cinder_netapp,
+  $nfs_shares_config      = '/etc/cinder/shares.conf',
+  $netapp_webservice_path = '/devmgr/v2',
 ) {
 
   include plugin_cinder_netapp::params
   include cinder::params
 
-  if ($cinder_netapp['multibackend']) {
-    $enabled_backends = "$backend_name,$netapp_backend_name"
-  } else {
-    $enabled_backends = $netapp_backend_name
-  }
-
+  # Sets correct parameter for <host_type> depending on storage family
   if ($cinder_netapp['netapp_storage_family']) == 'eseries' {
     $host_type = $cinder_netapp['netapp_eseries_host_type']
   } else {
     $host_type = $cinder_netapp['netapp_host_type']
   }
 
+  # NetApp driver does not understand boolean types for <lun_space_reservation> parameter
   if ($cinder_netapp['netapp_lun_space_reservation']) {
     $lun_space_reservation = 'enabled'
   } else {
     $lun_space_reservation = 'disabled'
   }
 
-  # To set up NFS shares
+  # Sets up NFS shares
   if ($cinder_netapp['netapp_storage_protocol']) == 'nfs' {
     package { 'nfs-common': }
 
@@ -40,7 +36,7 @@ define plugin_cinder_netapp::backend::netapp (
     }
   }
 
-  # To have an ability to create a root volume during an instance spawning
+  # We need following packages to create a root volume during an instance spawning
   if ($cinder_netapp['netapp_storage_protocol']) == 'iscsi' {
     package { 'open-iscsi': }
 
@@ -49,14 +45,13 @@ define plugin_cinder_netapp::backend::netapp (
     }
   }
 
-  # Ensure that $ symbol is correctly escaped in netapp password
+  # To be ensure that $ symbol is correctly escaped in netapp password
   $netapp_password = regsubst($cinder_netapp['netapp_password'],'\$','$$','G')
 
-  Cinder_config <||> ~> service { "$cinder::params::scheduler_service": } ~> service { "$cinder::params::volume_service": }
+
+  Cinder_config <||> -> Plugin_cinder_netapp::Backend::Enable_backend[$netapp_backend_name] ~> Service <||>
 
   cinder_config {
-    'DEFAULT/enabled_backends':                             value => $enabled_backends;
-    'DEFAULT/scheduler_driver':                             value => 'cinder.scheduler.filter_scheduler.FilterScheduler';
     "$netapp_backend_name/volume_backend_name":             value => 'cinder_netapp';
     "$netapp_backend_name/volume_driver":                   value => 'cinder.volume.drivers.netapp.common.NetAppDriver';
     "$netapp_backend_name/netapp_login":                    value => $cinder_netapp['netapp_login'];
@@ -70,7 +65,7 @@ define plugin_cinder_netapp::backend::netapp (
     "$netapp_backend_name/netapp_vfiler":                   value => $cinder_netapp['netapp_vfiler'];
     "$netapp_backend_name/netapp_controller_ips":           value => $cinder_netapp['netapp_controller_ips'];
     "$netapp_backend_name/netapp_sa_password":              value => $cinder_netapp['netapp_sa_password'];
-    "$netapp_backend_name/netapp_webservice_path":          value => '/devmgr/v2';
+    "$netapp_backend_name/netapp_webservice_path":          value => $netapp_webservice_path;
     "$netapp_backend_name/nfs_shares_config":               value => $nfs_shares_config;
     "$netapp_backend_name/thres_avl_size_perc_start":       value => $cinder_netapp['thres_avl_size_perc_start'];
     "$netapp_backend_name/thres_avl_size_perc_stop":        value => $cinder_netapp['thres_avl_size_perc_stop'];
@@ -88,4 +83,8 @@ define plugin_cinder_netapp::backend::netapp (
     "$netapp_backend_name/backend_host":                    value => 'str:netapp'; # for NetApp HA
   }
 
+  # Adds the backend in <enabled_backends> parameter
+  plugin_cinder_netapp::backend::enable_backend { $netapp_backend_name: }
+
+  service { $cinder::params::volume_service: }
 }
