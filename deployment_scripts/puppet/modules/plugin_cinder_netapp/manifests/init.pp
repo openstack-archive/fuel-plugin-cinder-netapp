fd77da1641cacd14d87b7063778ea584e336e84a
Roman Sokolkov <rsokolkov@mirantis.com>
2016-07-04 12:02:07 +0300

    Added MOS 9.0 support
      - Added netapp_cinder_controller task
        that will run only if ceph enabled.
        Before logic was in puppet.
      - netapp_hiera_override should NOT be run
        on cinder role (cinder-volume) as it
        required only for backend creation
        which is done on controller.
      - Changed default backends logic in init.pp.
        Use real MOS backends, not override them.
        So removed unused rbd.pp and iscsi.pp.
      - Added basic LCM support.
    
    Change-Id: Ia885d1a931f2111bdd3e9e1dccec07d34e42d2f0

--- deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/init.pp
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/init.pp
@@ -1,18 +1,15 @@
 class plugin_cinder_netapp (
   $cinder_netapp        = $plugin_cinder_netapp::params::cinder_netapp,
-  $backend_name         = $plugin_cinder_netapp::params::backend_name,
-  $backend_type         = $plugin_cinder_netapp::params::backend_type,
-  $backend_class        = $plugin_cinder_netapp::params::backend_class,
   $netapp_backend_class = $plugin_cinder_netapp::params::netapp_backend_class,
 ) inherits plugin_cinder_netapp::params {
 
-  if ($cinder_netapp['default_backend']) {
+  # If not selected disable default MOS backends
+  if ! $cinder_netapp['default_backend'] {
 
-    Class[$backend_class] -> Class[$netapp_backend_class]
+    Cinder_config['DEFAULT/enabled_backends'] -> Class[$netapp_backend_class]
 
-    class { $backend_class:
-      backend_type => $backend_type,
-      backend_name => $backend_name,
+    cinder_config {
+      'DEFAULT/enabled_backends': ensure => absent;
     }
   }
 
