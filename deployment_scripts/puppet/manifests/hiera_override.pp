8636535f4ec3686d91d91722480afc6c6654da1c
Roman Sokolkov <rsokolkov@mirantis.com>
2016-07-12 12:12:21 +0300

    Workaround for fuel-library bug
    https://bugs.launchpad.net/fuel/+bug/1598163
    
    Change-Id: Ie9b6afc39395437c0bc85eada9bd24725b13159c

--- deployment_scripts/puppet/manifests/hiera_override.pp
+++ deployment_scripts/puppet/manifests/hiera_override.pp
@@ -16,3 +16,11 @@ file { "${hiera_dir}/${plugin_yaml}":
   ensure  => file,
   content => template('plugin_cinder_netapp/cinder_netapp.yaml.erb'),
 }
+
+# Workaround for bug 1598163
+exec { 'patch_puppet_bug_1598163':
+  path    => '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin',
+  cwd     => '/etc/puppet/modules/osnailyfacter/manifests/globals',
+  command => "sed -i \"s/hiera('storage/hiera_hash('storage/\" globals.pp",
+  onlyif  => "grep \"hiera('storage\" globals.pp"
+}
