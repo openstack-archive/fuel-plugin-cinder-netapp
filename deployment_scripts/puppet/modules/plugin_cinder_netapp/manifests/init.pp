ab9e282208735a3651ff88458eb789da99cb8e50
Andrey Volochay <avolochay@mirantis.com>
2016-10-15 17:24:01 +0300

    Add support for using up to 3 devices simultaneously
     - The number of simultaneously supported devices has been increased
       up to 3 NetApp devices
     - ONTAP 7-Mode support has been disabled
     - SolidFire support has been added
    
    Change-Id: Iae2d5a78e262a3e7dc3d8fbb02fba36b56b5ba01

--- deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/init.pp
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/init.pp
@@ -1,5 +1,10 @@
 class plugin_cinder_netapp (
+  $cinder_netapp = $plugin_cinder_netapp::params::cinder_netapp,
 ) inherits plugin_cinder_netapp::params {
 
-  class { $netapp_backend_class: }
+  $backends = range('1', $cinder_netapp['number_netapp_backends'])
+
+  plugin_cinder_netapp::backend::netapp { $backends : }
+
+  plugin_cinder_netapp::backend::solidfire { $backends : }
 }
