361cbff64836a974b88f20b9866ef927feeecc9d
Andrey Volochay <avolochay@mirantis.com>
2016-10-15 17:24:01 +0300

    Add support for using up to 3 devices simultaneously
     - The number of simultaneously supported devices has been increased
       up to 3 NetApp devices
     - ONTAP 7-Mode support has been disabled
     - SolidFire support has been added
    
    Change-Id: Iae2d5a78e262a3e7dc3d8fbb02fba36b56b5ba01

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -6,21 +6,16 @@ $cinder_netapp = hiera_hash('cinder_netapp', {})
 
 Nova_config <||> ~> Service <||>
 
+package { 'multipath-tools': }
+package { 'nfs-common': }
+
 nova_config {
   'libvirt/iscsi_use_multipath': value => $cinder_netapp['use_multipath_for_image_xfer'];
 }
 
-if ($cinder_netapp['netapp_storage_protocol']) == 'iscsi' and ($cinder_netapp['use_multipath_for_image_xfer']) {
-  package { 'multipath-tools': }
-}
-
-if ($cinder_netapp['netapp_storage_protocol']) == 'nfs' {
-  package { 'nfs-common': }
-
-  if $cinder_netapp['nfs_mount_options'] {
-    nova_config {
-      'libvirt/nfs_mount_options': value => $cinder_netapp['nfs_mount_options'];
-    }
+if $cinder_netapp['nfs_mount_options'] {
+  nova_config {
+    'libvirt/nfs_mount_options': value => $cinder_netapp['nfs_mount_options'];
   }
 }
 
---
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/manifests/backend/solidfire.pp
@@ -0,0 +1,54 @@
+#    Copyright 2015 SolidFire, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+
+define plugin_cinder_netapp::backend::solidfire (
+  $netapp_backend_name    = "netapp_backend_$name",
+  $cinder_netapp          = $plugin_cinder_netapp::params::cinder_netapp,
+) {
+
+if ($cinder_netapp["netapp_storage_family$name"] in ['solidfire']) {
+
+  include cinder::params
+  include cinder::client
+
+  if ! defined( Package['open-iscsi'] ) {
+    package { 'open-iscsi': }
+  }
+
+  Cinder::Backend::Solidfire <||> -> Plugin_cinder_netapp::Backend::Enable_backend[$netapp_backend_name] ~> Service <||>
+
+  cinder::backend::solidfire { $netapp_backend_name :
+    san_ip                    => $cinder_netapp["solidfire_mvip$name"],
+    san_login                 => $cinder_netapp["solidfire_admin_login$name"],
+    san_password              => $cinder_netapp["solidfire_admin_password$name"],
+    volume_backend_name       => "cinder_netapp_backend_$name",
+    sf_emulate_512            => 'true',
+    sf_api_port               => $cinder_netapp["solidfire_api_port$name"],
+    sf_account_prefix         => $cinder_netapp["solidfire_account_prefix$name"],
+    sf_allow_template_caching => $cinder_netapp["solidfire_allow_template_caching$name"],
+    sf_template_account_name  => $cinder_netapp["solidfire_template_account$name"],
+    sf_volume_prefix          => $cinder_netapp["solidfire_volume_prefix$name"],
+    extra_options             => { "${netapp_backend_name}/backend_host" => { value => 'str:netapp' },},
+  }
+
+  # Adds the backend in <enabled_backends> parameter
+  plugin_cinder_netapp::backend::enable_backend { $netapp_backend_name: }
+
+  if ! defined( Service ) {
+    service { $cinder::params::volume_service: }
+  }
+}
+}
