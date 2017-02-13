e2d19f83e6777412b29b1c30b3dc468dce5a82bf
Felipe Alfaro Solana <felipe.alfarosolana@telefonica.com>
2017-02-13 12:55:10 +0100

    Cinder/NetApp plug-in 7.0.0:
    - Support multiple shares on the same NetApp back-end using different
      IPv4 addresses.
    - Remove NFS share IPv4 address (now specified in the share itself).

--- deployment_scripts/puppet/modules/plugin_cinder_netapp/lib/puppet/parser/functions/get_nfs_shares.rb
+++ deployment_scripts/puppet/modules/plugin_cinder_netapp/lib/puppet/parser/functions/get_nfs_shares.rb
@@ -3,10 +3,9 @@ module Puppet::Parser::Functions
     data = args[0]
     backend_number = args[1]
     nb_share = data['nb_share' + backend_number.to_s].to_i
-    ip = data['nfs_server_ip' + backend_number.to_s]
     shares = ''
     nb_share.downto(1) { |share|
-      shares += ip + ':' + data['nfs_server_share' + backend_number.to_s + share.to_s] + "\n"
+      shares += data['nfs_server_share' + backend_number.to_s + share.to_s] + "\n"
     }
     return shares
   end
